f35adf10834e06d32fbed0c27ae0ea61887d7e53
Nomis89 <95224816+Nomis89@users.noreply.github.com>
2022-08-23 09:36:18 +0200

    Eerste Commit

---
+++ src/main/java/nl/recepten/app/ReceptenAppApplication.java
@@ -0,0 +1,13 @@
+package nl.recepten.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/recepten/app/ReceptenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.recepten.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
