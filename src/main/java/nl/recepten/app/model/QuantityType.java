af0f6419b70cbc84e53388eb0d31dcc51e708f61
Elardo <erudolphy@hotmail.nl>
2022-08-25 15:46:05 +0200

    QuantityTypes toegevoegd

--- src/main/java/nl/recepten/app/model/QuantityType.java
+++ src/main/java/nl/recepten/app/model/QuantityType.java
@@ -1,29 +1,5 @@
 package nl.recepten.app.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class QuantityType {
-
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	
-	@Column(name = "QuantityName")
-	private String quantityName;
-
-	public String getQuantityName() {
-		return quantityName;
-	}
-
-	public void setQuantityName(String quantityNameValue) {
-		quantityName = quantityNameValue;
-	}
-	
-	
+public enum QuantityType {
+	KILO, GRAM, THEELEPEL, EETLEPEL, SNEE, KOPJE, PAKJE, ZAKJE, FLESJE, STUK, SNUFJE, NAAR_SMAAK, LITER, MILLILITER
 }
--- src/main/java/nl/recepten/app/rest/QuantityTypeEndpoint.java
+++ src/main/java/nl/recepten/app/rest/QuantityTypeEndpoint.java
@@ -1,23 +1,21 @@
 package nl.recepten.app.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import nl.recepten.app.persistence.QuantityTypeService;
+import nl.recepten.app.model.QuantityType;
+
+
 
 @RestController
 public class QuantityTypeEndpoint {
 
-	@Autowired
-	QuantityTypeService qts;
 	
-	@GetMapping("/qtsTest/{mijnInput}")
-	public String saveqts(@PathVariable("mijnInput") String value) {
-		System.out.println("saveqts");
-		qts.saveQTR(value);
-		return "qts opslaan succesvol";
+	@GetMapping("/returnQuantityTypes")
+	public QuantityType[] returnQuantityTypes() {
+		System.out.println("returnQuantityTypes");
+		return QuantityType.values();
 	}
 	
 }
