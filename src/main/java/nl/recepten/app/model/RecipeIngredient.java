36bde56226ea90d280a923ef488d9ad3e6f58054
Elardo <erudolphy@hotmail.nl>
2022-08-25 16:30:15 +0200

    RecipeIngredient af

--- src/main/java/nl/recepten/app/model/RecipeIngredient.java
+++ src/main/java/nl/recepten/app/model/RecipeIngredient.java
@@ -16,9 +16,35 @@ public class RecipeIngredient {
 	@ManyToOne(optional = false)
 	private Recept recipe;
 	
+	@ManyToOne(optional = false)
+	private Ingredient ingredient;
 	
-	
-	private int amount;
+	private double amount;
 	private QuantityType amountType;
+	
+	public Recept getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recept recipe) {
+		this.recipe = recipe;
+	}
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public QuantityType getAmountType() {
+		return amountType;
+	}
+	public void setAmountType(QuantityType amountType) {
+		this.amountType = amountType;
+	}
 	
 }
