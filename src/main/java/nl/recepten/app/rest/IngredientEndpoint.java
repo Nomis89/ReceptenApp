beb7b299a1a63cf123bcba5762f694cea3bb3df3
WENNPBLE <smitswen@gmail.com>
2022-08-31 10:23:04 +0200

    endpoint ingredient by id

--- src/main/java/nl/recepten/app/rest/IngredientEndpoint.java
+++ src/main/java/nl/recepten/app/rest/IngredientEndpoint.java
@@ -1,7 +1,10 @@
 package nl.recepten.app.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.recepten.app.model.Ingredient;
@@ -18,4 +21,16 @@ public class IngredientEndpoint {
 			System.out.println("returnIngredients");
 			return is.giveAllIngredients();
 		}
+		
+		@GetMapping("/{id}")
+		public Ingredient getIngredientById(@PathVariable("id")long id) {
+			System.out.println("Hij doet het W");
+			int x;
+			Ingredient ingredient = new Ingredient();
+//			ingredient.setId(28);
+			return is.serviceGetIngredientById(id);
+//			return ingredient;
+		}
+		
+
 }
