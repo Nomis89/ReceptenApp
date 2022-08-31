975ed0b3374f6062db51ea18c96806aa1a0df5fd
Elardo <erudolphy@hotmail.nl>
2022-08-31 11:26:48 +0200

    Hibernate toegevoegd

--- src/main/java/nl/recepten/app/model/Recept.java
+++ src/main/java/nl/recepten/app/model/Recept.java
@@ -3,11 +3,16 @@ package nl.recepten.app.model;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 
 @Entity
@@ -21,12 +26,17 @@ public class Recept {
 	private String instructions;
 	private int cookingTime;
 	private int totalPortions;
-	private KitchenAppliance kitchenAppliance;
+	
+	@ElementCollection(targetClass = KitchenAppliance.class)
+	@JoinTable(name = "recipeKitchenAppliance", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "KitchenAppliance", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private List<KitchenAppliance> kitchenAppliance;
 
-	public KitchenAppliance getKitchenAppliance() {
+	public List<KitchenAppliance> getKitchenAppliance() {
 		return kitchenAppliance;
 	}
-	public void setKitchenAppliance(KitchenAppliance kitchenAppliance) {
+	public void setKitchenAppliance(List<KitchenAppliance> kitchenAppliance) {
 		this.kitchenAppliance = kitchenAppliance;
 	}
 	private boolean vegitarian;
