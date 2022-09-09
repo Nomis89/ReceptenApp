332244badcc01be3047847674ea0898b87674912
drewingtonC137 <amjj.edwards@gmail.com>
2022-09-09 15:31:59 +0200

    userEditUpdate

--- src/main/java/nl/recepten/app/rest/UserEndpoint.java
+++ src/main/java/nl/recepten/app/rest/UserEndpoint.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +37,19 @@ public class UserEndpoint {
 		return loggedInUser;
 	}
 	
+	@PutMapping("updateUser/{id}")
+	public void updateUserById(@PathVariable("accountid")long accountId, @RequestBody User user) {
+		Account loggedInAccount = as.accountSession(accountId);
+		User loggedInUser = loggedInAccount.getUser();
+		loggedInUser.setCityName(user.getCityName());
+		loggedInUser.setDescription(user.getDescription());
+		loggedInUser.setStreetName(user.getStreetName());
+		loggedInUser.setName(user.getName());
+		loggedInUser.setCityName(user.getCityName());
+		loggedInUser.setZipCode(user.getZipCode());
+		loggedInUser.setHouseNumber(user.getHouseNumber());
+		loggedInUser.setHouseNumberAddition(user.getHouseNumberAddition());
+	}
 	@PostMapping("adduser")
 	public void addUser(@RequestBody User gebruiker) {
 		System.out.println(gebruiker.getName());
