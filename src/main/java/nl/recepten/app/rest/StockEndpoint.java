89d18c6ea73ccee8fbd878e45aa3b92754118f45
Elardo <erudolphy@hotmail.nl>
2022-09-09 09:13:36 +0200

    added functionality to get a users stock

--- src/main/java/nl/recepten/app/rest/StockEndpoint.java
+++ src/main/java/nl/recepten/app/rest/StockEndpoint.java
@@ -2,11 +2,15 @@ package nl.recepten.app.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.recepten.app.model.Account;
 import nl.recepten.app.model.Stock;
+import nl.recepten.app.persistence.AccountService;
+import nl.recepten.app.persistence.StockRepository;
 import nl.recepten.app.persistence.StockService;
 
 @RestController
@@ -14,6 +18,12 @@ public class StockEndpoint {
 	
 	@Autowired
 	StockService stockservice; 
+	
+	@Autowired
+	StockRepository stockrepository;
+	
+	@Autowired
+	AccountService as;
 	
 	@GetMapping("/allstock")
 	public Iterable<Stock> allStock(){
@@ -25,5 +35,13 @@ public class StockEndpoint {
 	public void addStock(@RequestBody Stock stock) {
 		stockservice.saveStock(stock);
 
-}
+	}
+	
+	@GetMapping("/stockFromAccount/{id}")
+	public Iterable<Stock> stockFromAccount (@PathVariable("id") long id){
+		
+		Account account = as.accountSession(id);
+		return stockrepository.findByuser(account.getUser());
+
+	}
 }
